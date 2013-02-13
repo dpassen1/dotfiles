3e594f45b1f221b823b2f9a9ed60e8ee275335c0
Derek Passen <dpassen1@gmail.com>
2013-02-13 11:53:25 -0500

    Only pull if the repo is clean.

--- update-all.sh
+++ update-all.sh
@@ -3,16 +3,22 @@
 clear
 for dir in `ls -d */`; do
     cd $dir
-        if [ -d ".hg" ]; then
-            echo "Mercurial: $dir"
+    if [ -d ".hg" ]; then
+        echo "Mercurial: $dir"
+        if [ -z "`hg status -admrn`" ]; then
+            echo "Clean. Pulling ..."
             hg pull -u 1>/dev/null
-            hg lg | head -n 2
-            echo
-        elif [ -d ".git" ]; then
-            echo "Git: $dir"
+        fi  
+        hg lg | head -n 2
+        echo
+    elif [ -d ".git" ]; then
+        echo "Git: $dir"
+        if [ -z "`git status --short`" ]; then
+            echo "Clean. Pulling ..."
             git pull --rebase 1>/dev/null
-            git lg | head -n 1
-            echo
-        fi
+        fi  
+        git lg | head -n 1
+        echo
+    fi  
     cd ..
 done
