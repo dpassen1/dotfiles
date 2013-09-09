b8c55b8fa7229ac34a32695d2b214d138a42629a
Derek Passen <dpassen1@gmail.com>
2013-09-08 22:31:30 -0400

    Trim trailing slashes for aesthetics.

--- update-all.sh
+++ update-all.sh
@@ -4,7 +4,7 @@ clear
 for dir in `ls -d */ | sort -f`; do
     cd $dir
     if [ -d ".hg" ]; then
-        echo "Mercurial: $dir"
+        echo "Mercurial: ${dir%/}"
         if [ -z "`hg status -admrn`" ]; then
             echo "Clean. Pulling ..."
             hg pull --rebase 1>/dev/null
@@ -12,7 +12,7 @@ for dir in `ls -d */ | sort -f`; do
         hg lg | head -n 1
         echo
     elif [ -d ".git" ]; then
-        echo "Git: $dir"
+        echo "Git: ${dir%/}"
         if [ -z "`git status --short`" ]; then
             echo "Clean. Pulling ..."
             git pull --rebase 1>/dev/null
