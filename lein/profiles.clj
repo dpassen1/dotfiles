7e23528442323bb62d60386252e7fc9e84be9628
Derek Passen <dpassen1@gmail.com>
2015-02-12 18:10:05 -0600

    lein: Add lein-kibit, eastwood, and cljfmt

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,4 @@
-{:user {:plugins [[lein-midje "3.1.3"]]}}
+{:user {:plugins [[lein-midje      "3.1.3"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit      "0.0.8"]
+                  [lein-cljfmt     "0.1.7"]]}}
