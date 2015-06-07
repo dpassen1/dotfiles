88e04557f685d1f59f05e905793f2b25d51cef8a
Derek Passen <dpassen1@gmail.com>
2015-06-06 20:31:02 -0500

    Clojure: Bring in cider-nrepl for ease of dev in Emacs.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[lein-midje      "3.1.3"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-kibit      "0.0.8"]
-                  [lein-cljfmt     "0.1.7"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+                  [lein-midje        "3.1.3"]
+                  [jonase/eastwood   "0.2.1"]
+                  [lein-kibit        "0.0.8"]
+                  [lein-cljfmt       "0.1.7"]]}}
