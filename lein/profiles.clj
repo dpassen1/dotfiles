2144c05ad9d57c95d3e44a1ff456108c0aa78a0b
Derek Passen <dpassen1@gmail.com>
2015-06-20 17:18:51 -0500

    LEININGEN: Update utility libraries.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-midje        "3.1.3"]
                   [jonase/eastwood   "0.2.1"]
-                  [lein-kibit        "0.0.8"]
-                  [lein-cljfmt       "0.1.7"]]}}
+                  [lein-kibit        "0.1.2"]
+                  [lein-cljfmt       "0.1.10"]]}}
