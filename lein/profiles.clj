d74f9a820b81bc501abc842e8180adc90a2dfde2
Derek Passen <dpassen1@gmail.com>
2015-07-28 20:27:39 -0500

    LEIN: Upgrade lein-cljfmt.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [jonase/eastwood   "0.2.1"]
                   [lein-ancient      "0.6.7"]
-                  [lein-cljfmt       "0.2.1"]
+                  [lein-cljfmt       "0.3.0"]
                   [lein-kibit        "0.1.2"]
                   [lein-midje        "3.1.3"]
                   [lein-try          "0.4.3"]]}}
