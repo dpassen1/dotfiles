9436828e82831151f3a8ecbb8bce8b36d9efeb64
Derek Passen <dpassen1@gmail.com>
2015-07-28 20:27:26 -0500

    LEIN: Add lein-ancient to user profile.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [jonase/eastwood   "0.2.1"]
+                  [lein-ancient      "0.6.7"]
                   [lein-cljfmt       "0.2.1"]
                   [lein-kibit        "0.1.2"]
                   [lein-midje        "3.1.3"]
