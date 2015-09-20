1f926e7557da4169feb9937b34b1d9b9efb1514c
Derek Passen <dpassen1@gmail.com>
2015-09-19 22:24:46 -0500

    More robust all-git.sh script.

--- all-git.sh
+++ all-git.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
-for dir in $(ls -d */ | sort -f); do
+set -o nounset
+set -o errexit
+
+find . -type d -d 1 | while read dir; do
     if [ -d "$dir/.git" ]; then
         echo "Git: ${dir%/}"
-        (cd $dir && git $@)
+        (cd "$dir" && git "$@")
         echo
     fi
 done
