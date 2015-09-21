354f84824389828f59c34c2367130cd454fbc881
Derek Passen <dpassen1@gmail.com>
2015-09-21 09:20:50 -0500

    Since GNU find and BSD find act differently, let's use ls.

--- all-git.sh
+++ all-git.sh
@@ -3,7 +3,7 @@
 set -o nounset
 set -o errexit
 
-find . -type d -d 1 | while read dir; do
+ls -d */ | sort -f | while read dir; do
     if [ -d "$dir/.git" ]; then
         echo "Git: ${dir%/}"
         (cd "$dir" && git "$@")
