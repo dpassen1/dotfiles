04cfb16f5bd250b0c8828bbac72d94d5e273579c
Derek Passen <dpassen1@gmail.com>
2015-11-18 21:07:01 -0600

    lein: move cider/cider-nrepl plugin into the repl profile.
    - Taken from http://blog.maio.cz/2015/11/cider-slows-down-leiningen-startup-here.html

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [jonase/eastwood   "0.2.1"]
+{:user {:plugins [[jonase/eastwood   "0.2.1"]
                   [lein-ancient      "0.6.8"]
                   [lein-cljfmt       "0.3.0"]
                   [lein-kibit        "0.1.2"]
                   [lein-midje        "3.2"]
-                  [lein-try          "0.4.3"]]}}
+                  [lein-try          "0.4.3"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.9.1"]]}}
