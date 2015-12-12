d9f2c04434b0d5ae13093ebee8686f2e0dc85411
Derek Passen <dpassen1@gmail.com>
2015-12-12 17:45:46 -0600

    A probably-flawed link-all script.
    - Need to figure out how to handle Vundle better.

---
+++ link-all.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+function ensure_destination_exists {
+    local dest="$1"
+    if ! [[ $dest == "." ]]; then
+        dest="~/."$dest""
+        echo "  Creating destination directory: "$dest""
+        mkdir -p "$dest"
+    fi
+}
+
+function create_symbolic_link {
+    local path="$1"
+    local pwd=$(pwd)
+    local src="$pwd/$path"
+    local dest_path=${path/%\.symlink/}
+    local dest="$HOME/.$dest_path"
+    ln -fs "$src" "$dest"
+}
+
+function link_dotfiles {
+    find . -iname "*.symlink" | while read link; do
+        echo "Linking "$link""
+
+        local relpath=${link/#\.\//}
+        local dir=$(dirname "$relpath")
+
+        ensure_destination_exists $dir
+        create_symbolic_link $relpath
+    done
+}
+
+link_dotfiles
