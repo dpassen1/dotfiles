985fcddb319a8c6e56b175a7e6bd90f6510f47e1
Derek Passen <dpassen1@gmail.com>
2017-10-12 17:59:13 -0500

    all-git: No reason to add trailing slash here.

--- all-git.sh
+++ all-git.sh
@@ -3,7 +3,7 @@
 set -o nounset
 set -o errexit
 
-ls -d */ | sort -f | while read dir; do
+ls -d * | sort -f | while read dir; do
     if [ -d "$dir/.git" ]; then
         echo "${dir%/}"
         (cd "$dir" && git "$@")
