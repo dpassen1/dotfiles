11351ecdce20f830295cff1f05a78e0699bed7a2
Derek Passen <dpassen1@gmail.com>
2018-02-24 12:07:20 -0600

    all-git cleanup.

--- all-git.sh
+++ all-git.sh
@@ -3,7 +3,7 @@
 set -o nounset
 set -o errexit
 
-ls -d */ | sort -f | while read dir; do
+find . -maxdepth 1 -type d | sort -f | while read -r dir; do
     if [ -d "$dir/.git" ]; then
         echo "${dir%/}"
         (cd "$dir" && git "$@")
