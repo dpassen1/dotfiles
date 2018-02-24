6d55801153bc648a7f8e49cf8b5506f0a4f8c59c
Derek Passen <dpassen1@gmail.com>
2018-02-24 14:34:29 -0600

    link-all: Remove unnecessary quoting.

--- all-git.sh
+++ all-git.sh
@@ -6,7 +6,7 @@ set -o errexit
 find . -maxdepth 1 -type d | sort -f | while read -r dir; do
     if [ -d "$dir/.git" ]; then
         echo "${dir%/}"
-        (cd "$dir" && git "$@")
+        (cd "$dir" && git $@)
         echo
     fi
 done
