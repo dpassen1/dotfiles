2411739d6b55c6ec5f11fc44e302d4ccefb0cea0
Derek Passen <dpassen1@gmail.com>
2018-03-09 17:44:57 -0600

    all-git: Linter fixes.

--- all-git.sh
+++ all-git.sh
@@ -5,8 +5,8 @@ set -o errexit
 
 find . -maxdepth 1 -type d | sort -f | while read -r dir; do
     if [ -d "$dir/.git" ]; then
-        echo "${dir%/}"
-        (cd "$dir" && git $@)
+        basename "${dir%/}"
+        (cd "$dir" && git "$@")
         echo
     fi
 done
