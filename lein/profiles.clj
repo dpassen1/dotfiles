7374889cd98edd6b3e2b690c37b11d530ca3e866
Derek Passen <dpassen1@gmail.com>
2021-01-24 12:36:28 -0600

    lein: Upgrade lein-ancient to latest

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins
   [[com.jakemccrary/lein-test-refresh "0.24.1"]
-   [lein-ancient                      "0.6.15"]
+   [lein-ancient                      "0.7.0"]
    [lein-try                          "0.4.3"]]
   :test-refresh {:quiet true}}}
